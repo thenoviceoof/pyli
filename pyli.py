4d2ae93266b71123618768230c6b593ee3952753
Nathan Hwang <thenoviceoof@gmail.com>
2013-02-28 00:25:35 -0500

    Get code to execute, with inserted imports

--- pyli.py
+++ pyli.py
@@ -5,15 +5,40 @@ import symbol
 import sys
 from pprint import pprint
 
-tree = parser.st2tuple(parser.suite(sys.argv[1]))
-
 def convert_readable(tree):
-    return tuple(((token.tok_name[i]
-                   if token.tok_name.get(i) else
-                   symbol.sym_name[i])
-                  if isinstance(i, int) else
-                  (i if isinstance(i, str) else convert_readable(i)))
-                 for i in tree)
+    def int2sym(i):
+        if isinstance(i, int):
+            if token.tok_name.get(i) is not None:
+                return token.tok_name[i]
+            else:
+                return symbol.sym_name[i]
+        else:
+            if isinstance(i, basestring):
+                return i
+            else:
+                return convert_readable(i)
+    return tuple(int2sym(i) for i in tree)
+
+rtok_name = dict((v,k) for k,v in token.tok_name.iteritems())
+rsym_name = dict((v,k) for k,v in symbol.sym_name.iteritems())
+
+def convert_numeric(tree):
+    def sym2int(s):
+        if isinstance(s, basestring):
+            if rtok_name.get(s) is not None:
+                return rtok_name[s]
+            elif rsym_name.get(s) is not None:
+                return rsym_name[s]
+            elif not s:
+                return s
+            else:
+                raise ValueError('Unexpected token %s' % s)
+        elif isinstance(s, tuple):
+            if rtok_name.get(s[0]) is not None:
+                return (sym2int(s[0]), s[1])
+            else:
+                return convert_numeric(s)
+    return tuple(sym2int(i) for i in tree)
 
 def find_tokens(tree):
     '''
@@ -39,6 +64,9 @@ def find_tokens(tree):
         i = (i for i,t in enumerate(tree[1:]) if t[0] == 'EQUAL').next()
         return (tuple(token for t in tree[i+1:] for token in find_tokens(t)[0]),
                 tuple(token for t in tree[:i] for token in find_tokens(t)[0]))
+    # handl
+    if tree[0] == 'trailer' and tree[1][0] == 'DOT':
+        return tuple(), tuple()
     # handles cases like ('NEWLINE, '')
     if all(isinstance(t, str) for t in tree):
         return tuple(), tuple()
@@ -49,6 +77,33 @@ def find_tokens(tree):
     bound = tuple(x for f in bound for x in f)
     return tuple(free), tuple(bound)
 
-read_tree = convert_readable(tree)
-pprint(read_tree)
-pprint(find_tokens(read_tree))
+def import_packages(tree, packages):
+    if tree[0] != 'file_input':
+        raise ValueError('This function must be given a full parse tree')
+    imports = [('stmt',
+                ('simple_stmt',
+                 ('small_stmt',
+                  ('import_stmt',
+                   ('import_name',
+                    ('NAME', 'import'),
+                    ('dotted_as_names',
+                     ('dotted_as_name',
+                      ('dotted_name',
+                       ('NAME', p))))))),
+                 ('NEWLINE', ''))) for p in packages]
+    ntree = tuple(['file_input'] + list(imports) + list(tree[1:]))
+    return ntree
+
+if __name__ == '__main__':
+    tree = parser.st2tuple(parser.suite(sys.argv[1]))
+    read_tree = convert_readable(tree)
+    pprint(read_tree)
+    free, bound = find_tokens(read_tree)
+    pprint((free, bound))
+    # right now assume that we want to try and import each free thing
+    free = list(set(free).difference(['print']))
+    read_tree = import_packages(read_tree, free)
+    tree = convert_numeric(read_tree)
+    p = parser.tuple2st(tree)
+    code = p.compile('test.py')
+    eval(code)
