98b0d103a10155142df3fce20425d3d5db4de603
Nathan Hwang <thenoviceoof@gmail.com>
2013-03-02 22:37:59 -0500

    Document the project before I run out of motivation

--- pyli.py
+++ pyli.py
@@ -1,3 +1,9 @@
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <thenoviceoof> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return
+#  - thenoviceoof
+
 import argparse
 import parser
 import token
