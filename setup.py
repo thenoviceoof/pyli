22646faa002a56e131cc421f4d2265d12c89f9dc
Nathan Hwang <thenoviceoof@gmail.com>
2013-03-10 22:34:22 -0400

    Package up pyli for pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pyli',
+    version='0.1.0',
+    author='thenoviceoof',
+    author_email='thenoviceoof@gmail.com',
+    packages=[],
+    scripts=['bin/pyli.py'],
+    url='https://github.com/thenoviceoof/pyli',
+    license='LICENSE',
+    description='Better python CLI integration',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
