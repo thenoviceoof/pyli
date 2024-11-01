c037b5a20e668b1d5c4503e7c7894061bd6e00cd
thenoviceoof <thenoviceoof@gmail.com>
2024-11-01 01:07:45 -0400

    Fix up actually inserting the passed variables

--- pyli/preamble.py
+++ pyli/preamble.py
@@ -6,5 +6,5 @@ def set_intro_variables(tree: ast.AST, variables: dict) -> None:
         new_node = ast.Assign(targets=[ast.Name(id=name, ctx=ast.Store())],
                               value=ast.Constant(value=value))
         # Order doesn't really matter, otherwise we wouldn't use a dict.
-        tree.body.insert(new_node, 0)
+        tree.body.insert(0, new_node)
     ast.fix_missing_locations(tree)
